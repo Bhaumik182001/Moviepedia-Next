3c0cb937fb1afb2211c12f0c10e8a60292f8b50f
Bhaumik <Bhaumik182001@gmail.com>
2026-09-06 16:32:25 +0530

    Refactor StarRating component to use SVGs

--- components/StarRating.js
+++ components/StarRating.js
@@ -1,32 +1,29 @@
 import React from "react";
-import { useState } from "react";
 
-const EMPTY_STAR =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Five-pointed_star.svg/1088px-Five-pointed_star.svg.png";
-const FULL_STAR =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Gold_Star.svg/1200px-Gold_Star.svg.png";
+const StarRating = ({ rating }) => {
+  const starIds = [1, 2, 3, 4, 5];
 
-
-const StarRating = ({rating}) => {
-    const starIds = [1, 2, 3, 4, 5];
-  
-    const getImg = (id) => {
-      return  rating >= id ? FULL_STAR : EMPTY_STAR;
-    };
-  
-    return (
-      <div
-        className="flex drop-shadow-lg shadow-black"
-      >
-        {starIds.map((id) => (
-          <img
-            key={id}
-            src={getImg(id)}
-            className="w-7 sm:w-10 lg:w-12"
+  return (
+    <div className="flex drop-shadow-lg shadow-black">
+      {starIds.map((id) => (
+        <svg
+          key={id}
+          className={`w-7 sm:w-10 lg:w-12 text-yellow-400 ${
+            rating >= id
+              ? "fill-current"
+              : "fill-transparent stroke-current stroke-2"
+          }`}
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
           />
-        ))}
-      </div>
-    );
-  };
+        </svg>
+      ))}
+    </div>
+  );
+};
 
-  export default StarRating;
+export default StarRating;
